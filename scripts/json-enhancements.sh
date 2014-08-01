0d3aa60053767064e31b54c19714c7a00d014cd1
Trevor Bramwell <trevor@bramwell.net>
2014-08-01 13:58:45 -0700

    Remove VIRTUAL_ENV from json-enhancements

--- scripts/json-enhancements.sh
+++ scripts/json-enhancements.sh
@@ -4,5 +4,5 @@
 # to be performed at system installation time, rather than
 # every time Socorro is built
 if [ ! -f `pg_config --pkglibdir`/json_enhancements.so ]; then
-    sudo env PATH=$PATH ${VIRTUAL_ENV}/bin/python -c "from pgxnclient import cli; cli.main(['install', 'json_enhancements'])"
+    sudo env PATH=$PATH python -c "from pgxnclient import cli; cli.main(['install', 'json_enhancements'])"
 fi
