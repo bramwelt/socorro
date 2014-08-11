9a41c972c34d97653e6a3b6c81a38d0fa00edf74
Trevor Bramwell <trevor@bramwell.net>
2014-08-11 16:07:53 -0700

    Change socorro-processor name to include dash.
    Add dependency on socorro-lib. This package doesn't not exist yet.

--- socorro-processor/setup.py
+++ socorro-processor/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="socorro_processor",
+    name="socorro-processor",
     description="processor...",
     author="Mozilla",
     author_email="socorro-dev@mozilla.com",
@@ -10,7 +10,10 @@ setup(
     url="http://github.com/mozilla/socorro",
     packages=['socorro_processor'],
     test_suite='nose.collector',
+    install_requires=[
+        'socorro-lib==0.1.0',
+    ],
     entry_points={
         'console_scripts': ['socorro-processor=socorro_processor.processor_app:app']
-        }
+    }
 )
