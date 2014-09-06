9f1b263c4960560ad826e7e3b7a84946872fd728
Trevor Bramwell <trevor@bramwell.net>
2014-09-05 19:37:31 -0700

    Add Packer Template
    The packer template will create a CentOS 6.5 VM using the puppet
    manifests for install socorro on Vagrant.

---
+++ packer/scripts/common/vagrant.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#date > /etc/vagrant_box_build_time
+
+mkdir /home/vagrant/.ssh
+wget --no-check-certificate \
+    'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
+    -O /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
